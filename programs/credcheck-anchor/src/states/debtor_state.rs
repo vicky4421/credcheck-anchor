9cbddeff316052d6d097618473de53d143352555
Vivek Sawant <vicky210490@gmail.com>
2024-11-01 20:47:41 +0530

    define debtor state

--- programs/credcheck-anchor/src/states/debtor_state.rs
+++ programs/credcheck-anchor/src/states/debtor_state.rs
@@ -1,2 +1,17 @@
 // Shree
 
+use anchor_lang::prelude::*;
+
+#[account]
+pub struct DebtorState {
+    pub gst_registration_number: String,        // for uniqueness
+    pub name: String,
+    pub average_default_days: u32,
+    pub defaulted_to: Vec<DefaultRecord>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct DefaultRecord {
+    pub creditor_pubkey: Pubkey,
+    pub default_days: u32,
+}
