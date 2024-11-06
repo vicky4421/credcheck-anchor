7287342fb0d38ad3e63a8c61d17a52aa7fddf898
Vivek Sawant <vicky210490@gmail.com>
2024-11-06 16:29:00 +0530

    add new field

--- programs/credcheck-anchor/src/states/transaction_state.rs
+++ programs/credcheck-anchor/src/states/transaction_state.rs
@@ -8,8 +8,9 @@ pub struct TransactionState {
     pub credit_days: u32,           // agreed credit days - 4 bytes
     pub transaction_date: i64,      // unix timestamp for last transaction - 8 bytes
     pub is_defaulted: bool,         // defaulted or not - 1 byte
+    pub is_cleared: bool,           // cleared or not - 1 byte
 }
 
 impl TransactionState {
-    pub const LEN: usize = 8 + 32 + 32 + 8 + 4 + 8 + 1;
+    pub const LEN: usize = 8 + 32 + 32 + 8 + 4 + 8 + 1 + 1;
 }
