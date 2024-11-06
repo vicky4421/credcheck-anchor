ab276a69fc62f2a71fdd946a7e4497f7b4267b83
Vivek Sawant <vicky210490@gmail.com>
2024-11-06 16:27:52 +0530

    use creditor state

--- programs/credcheck-anchor/src/states/mod.rs
+++ programs/credcheck-anchor/src/states/mod.rs
@@ -1,7 +1,9 @@
 pub mod debtor_state;
 pub mod default_state;
 pub mod transaction_state;
+pub mod creditor_state;
 
 pub use debtor_state::*;
 pub use default_state::*;
 pub use transaction_state::*;
+pub use creditor_state::*;
