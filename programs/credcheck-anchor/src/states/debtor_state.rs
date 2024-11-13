4068d399c14bed2bfc1383787976bad7a373b6ee
Vivek Sawant <vicky210490@gmail.com>
2024-11-13 15:40:33 +0530

    refactor debtor

--- programs/credcheck-anchor/src/states/debtor_state.rs
+++ programs/credcheck-anchor/src/states/debtor_state.rs
@@ -2,18 +2,17 @@
 
 use anchor_lang::prelude::*;
 
-use crate::states::transaction_state::TransactionState;
-
 #[account]
 pub struct DebtorState {
     pub gst_registration_number: String,        // for uniqueness - 15 chars - 4 for len + 15 bytes
     pub name: String,                           // max chars - 50 - 4 for len + 50 bytes
     pub average_default_days: u32,              // 4 bytes
-    pub defaulted_to: Vec<TransactionState>,    // pubkey of default transactions max 10 - 10 * 93 bytes + 4 bytes for len
-    pub last_updated: i64,                      // unix timestamp for last transaction - 8 bytes
+    pub total_transactions: u32,                // 4 bytes
+    pub defaulted_transactions: Vec<Pubkey>,    // pubkey of default transactions max 10 - 10 * 32 bytes + 4 bytes for len
+    pub transactions: Pubkey,                   // pubkey of first transactions list - 32 bytes
 }
 
 
 impl DebtorState {
-    pub const LEN: usize = 8 + 19 + 54 + 4 + 934 + 8;
+    pub const LEN: usize = 8 + 19 + 54 + 4 + 4 + 324 + 32;      // 445
 }
