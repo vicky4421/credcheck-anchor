c76bc15ccfe611c87d23007d8015d65ccf9354c7
Vivek Sawant <vicky210490@gmail.com>
2024-11-16 15:17:14 +0530

    refactor transaction list pda

--- programs/credcheck-anchor/src/states/transaction_list.rs
+++ programs/credcheck-anchor/src/states/transaction_list.rs
@@ -3,10 +3,11 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct TransactionList {
-    pub list: Vec<Pubkey>,    // 50 transactions per list: 32 * 50 = 1600
-    pub next_list: Option<Pubkey>,
+    pub list: Vec<Pubkey>,              // 50 transactions per list: 32 * 50 = 1600
+    pub is_full: bool,                  // 1 byte - true if list is full
+    pub next_list: Option<Pubkey>,      // 32 bytes - link to next list
 }
 
 impl TransactionList {
-    pub const LEN: usize = 8 + 1600 + 32;        // 1640
+    pub const LEN: usize = 8 + 1600 + 1 + 32;        // 1641
 }
